fc475b4f9669271719313a730cb352e613833246
james <28635265+JimMatthew@users.noreply.github.com>
2024-03-04 21:54:00 -0600

    add initial grid

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+function makeGrid() {
+
+    const containter = document.querySelector('#container');
+    const boxrow = document.createElement('div');
+    boxrow.classList.add('boxrow');
+    
+    for (let i = 0; i < 64; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.addEventListener('mouseover', function() {
+            box.style.backgroundColor = 'red';
+        });
+        boxrow.appendChild(box.cloneNode(true));
+    }
+    containter.appendChild(boxrow.cloneNode(true));
+    for (let j = 0; j < 64; j++) {
+        containter.appendChild(boxrow.cloneNode(true));
+    }
+
+}
+
+makeGrid();
