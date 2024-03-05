6fd3202a054492d80598e41ccbb0cb6cbd02bcbc
james <28635265+JimMatthew@users.noreply.github.com>
2024-03-05 07:29:33 -0600

    cleanup

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-
-
 function makeGrid() {
-
-    const a = document.querySelector('.header');
-
-    a.addEventListener('mouseover', event => {
-        
-    });
 
     const containter = document.querySelector('#container');
     const boxrow = document.createElement('div');
     boxrow.classList.add('boxrow');
-    
-    for (let i = 0; i < 64; i++) {
-        const box = document.createElement('div');
-        box.classList.add('box');
-        box.addEventListener('mouseover', (e) => {
-            box.target.style.backgroundColor = "red";
-            console.log("hovered");
-        });
-        boxrow.appendChild(box.cloneNode(true));
-    }
-    containter.appendChild(boxrow.cloneNode(true));
-    for (let j = 0; j < 64; j++) {
-        containter.appendChild(boxrow.cloneNode(true));
-    }
 
+    for (let i = 0; i < 64; i++) {
+        const boxrow = document.createElement('div');
+        boxrow.classList.add('boxrow');
+        for (let j = 0; j < 64; j++) {
+
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = "#06AED5";
+                console.log("hovered");
+            });
+            boxrow.appendChild(box);
+        }
+        containter.appendChild(boxrow);
+    }
 }
 
+function actions(){
+    const a = document.querySelector('.bclr');
+    a.addEventListener('click', clear);
+}
+
+function clear() {
+
+    const containter = document.querySelector('#container');
+    
+
+}
 makeGrid();
