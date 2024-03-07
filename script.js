d7642dd2956a7fe4af5d3c2b5acaebcf11c888f8
james <28635265+JimMatthew@users.noreply.github.com>
2024-03-06 22:40:10 -0600

    cleanup and add color wheel

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
+
+var isgrid = false;
+var numboxes = 32;
+const brightness=.1;
+var isMouseDown = false;
+var isEraseDown = false;
+var mode = 0;
+
+init();
+makeGrid();
+
+function init(){
+    document.querySelector('.btoggrid').addEventListener('click', togglegrid);
+    document.querySelector('.sizebox').value = numboxes;
+    document.querySelector('.bchangemode').addEventListener('click', changeMode);
+    document.querySelector('.bclr').addEventListener('click', makeGrid);
+    document.querySelector('.mainpanel').addEventListener("contextmenu", e => e.preventDefault());
+    document.addEventListener('mouseup', function(event) {
+        isMouseDown = false;
+        isEraseDown = false;
+    });
+}
+
 function makeGrid() {
-    nsize = document.querySelector('.sizebox').value;
+    numboxes = document.querySelector('.sizebox').value;
     const containter = document.querySelector('#container');
     containter.innerHTML = ''; //clear out the container
-    const boxrow = document.createElement('div');
-    boxrow.classList.add('boxrow');
-
-    for (let i = 0; i < nsize; i++) {
+    
+    for (let i = 0; i < numboxes; i++) {
         const boxrow = document.createElement('div');
         boxrow.classList.add('boxrow');
-        for (let j = 0; j < nsize; j++) {
+        for (let j = 0; j < numboxes; j++) {
             const box = document.createElement('div');
             box.classList.add('box');
             box.color = 255;
             box.addEventListener('mouseenter', handleModeAction);
-            container.addEventListener('mousedown', handleMouseDownAction);
+            box.addEventListener('mousedown', handleMouseDownAction);
             if (isgrid) {
                 box.style.border = '1px solid black'; 
               } else {
@@ -26,13 +47,11 @@ function makeGrid() {
 }
 
 function handleMouseDownAction(event) {
-
-
-    if (!ismousedown && !iscleardown){
+    if (!isMouseDown && !isEraseDown){
         if (event.button === 0) {
-            ismousedown = true;
+            isMouseDown = true;
         } else if (event.button === 2) {
-            iscleardown = true;
+            isEraseDown = true;
         }
         handleModeAction(event);
     }
@@ -47,71 +66,50 @@ function handleModeAction(event){
     }
 }
 
-function init(){
-    var tb = document.querySelector('.btoggrid');
-    tb.addEventListener('click', togglegrid);
-
-    var sb = document.querySelector('.sizebox');
-    sb.value = nsize;
-    const bcm = document.querySelector('.bchangemode');
-    bcm.addEventListener('click', changeMode);
-
-    const a = document.querySelector('.bclr');
-    a.addEventListener('click', makeGrid);
-
-    const cont = document.querySelector('#container');
-    cont.addEventListener("contextmenu", e => e.preventDefault());
-    
-    document.addEventListener('mouseup', function(event) {
-        ismousedown = false;
-        iscleardown = false;
-    });
-}
-
 function togglegrid() { 
-    var b = document.querySelector('.btoggrid');
+    var gridToggle = document.querySelector('.btoggrid');
     isgrid = !isgrid;
     if (isgrid){
-        b.textContent = "Grid On"
+        gridToggle.textContent = "Grid On"
     } else {
-        b.textContent = "Grid Off"
+        gridToggle.textContent = "Grid Off"
     }
-    var box = document.querySelectorAll('.box');
-    box.forEach(bx => {
+    document.querySelectorAll('.box').forEach(bx => {
         if (isgrid){
-            bx.style.border = "1px solid black"; 
+                bx.style.border = "1px solid black"; 
             } else {
-            bx.style.border = 'none';
+                bx.style.border = 'none';
             } 
     });
 }
 
 function changeMode(){
+    var colorpkr = document.querySelector('#bcolor');
+    var btn = document.querySelector('.bchangemode');
     if (mode == 0) {
         mode = 1;
+        colorpkr.style.display = 'block';
+        btn.textContent = "Color Mode";
     } else {
         mode = 0;
-    }
-    var lbl = document.querySelector('.cmodelbl');
-    if (mode == 0) {
-        lbl.textContent = "Current Mode: Shade";
-    } else {
-        lbl.textContent = "Current Mode: Color";
+        colorpkr.style.display = 'none';
+        btn.textContent = "Shade Mode";
     }
 }
 
 function mouseoverHandler(event) {
-     if(iscleardown && event.target.classList.contains('box')) {
+    var c = document.querySelector('#bcolor').value;
+     if(isEraseDown) {
         event.target.style.backgroundColor = "white";
-    } else if (ismousedown && event.target.classList.contains('box')) {
-        event.target.style.backgroundColor = "#06AED5";
+    } else if (isMouseDown) {
+        event.target.style.backgroundColor = c;
     }
 }
 
 function shadingHandler(event){
-    if (iscleardown){
+    if (isEraseDown){
        var mode = -1;
-    } else if (ismousedown) {
+    } else if (isMouseDown) {
         var mode = 1;
     } else {return}
     cell = event.target;
@@ -121,11 +119,3 @@ function shadingHandler(event){
     cell.style.backgroundColor = color;
 }
 
-var isgrid = false;
-var nsize = 32;
-const brightness=.1;
-var ismousedown = false;
-var iscleardown = false;
-var mode = 0;
-init();
-makeGrid();
